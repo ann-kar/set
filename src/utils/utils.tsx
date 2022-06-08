b8f2e1e6e7d19a9ff5bf79f3bd8893f47a040fad
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-06-08 19:01:07 +0200

    refactor: consolidate helpers in a class

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,4 +1,4 @@
-import { ICard, shapes, colors, fills, numbers } from "../ts/types";
+import { ICard } from "../ts/types";
 import { Card } from "../components";
 
 export class Utils {
@@ -11,68 +11,19 @@ export class Utils {
       number: features[3],
     };
   };
-}
 
-export function generateDeck() {
-  const deck: Array<ICard> = [];
-
-  for (const color of colors) {
-    for (const shape of shapes) {
-      for (const fill of fills) {
-        for (const number of numbers) {
-          deck.push({
-            id: `${color}-${shape}-${fill}-${number}`,
-            cardStatus: "Card-inactive",
-          });
-        }
-      }
+  static shuffleArray(array: Array<any>) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
   }
 
-  shuffleArray(deck);
-  return deck;
-}
-
-function shuffleArray(array: Array<any>) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+  static renderCards(cards: ICard[]) {
+    return cards.map((card: ICard) => {
+      return <Card key={card.id} id={card.id} cardStatus={card.cardStatus} />;
+    });
   }
-}
-
-export function check(cards: Array<string>): boolean {
-  const sorted = `${cards[0]}-${cards[1]}-${cards[2]}`.split("-").sort();
-  for (let i = 0; i < sorted.length; i++) {
-    if (
-      sorted[i] === sorted[i + 1] &&
-      sorted[i] !== sorted[i + 2] &&
-      i === sorted.indexOf(sorted[i])
-    ) {
-      // FUTURE ALERT: console.log(`You cannot have exactly two cards with ${sorted[i]} elements`);
-      return false;
-    }
-  }
-  return true;
-}
-
-export function checkAll(cards: Array<any>) {
-  cards = cards.map((card) => card.id);
-
-  for (let i = 0; i < cards.length; i++) {
-    for (let j = i + 1; j < cards.length; j++)
-      for (let k = j + 1; k < cards.length; k++) {
-        if (check([cards[i], cards[j], cards[k]])) {
-          return [cards[i], cards[j], cards[k]];
-        }
-      }
-  }
-  return null;
-}
-
-export function renderCards(cards: ICard[]) {
-  return cards.map((card: ICard) => {
-    return <Card key={card.id} id={card.id} cardStatus={card.cardStatus} />;
-  });
 }
