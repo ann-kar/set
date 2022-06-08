b114bbe88450d03fbdbf7c80778dba4da1a0e329
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-06-08 23:47:54 +0200

    fix: add key to TabWrapper

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,43 +1,42 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
-import './Main.scss';
-import { MenuItem, TabWrapper } from '../../components';
+import "./Main.scss";
+import { MenuItem, TabWrapper } from "../../components";
 
 function Main({ children }: any): JSX.Element {
-
-  const tabs = ['New Game', 'Rules', 'Options'];
+  const tabs = ["New Game", "Rules", "Options"];
 
   const [activeTab, setActiveTab] = useState(tabs[0]);
   const [menuHidden, setMenuHidden] = useState(true);
 
   const toggleMenu = () => {
-    console.log('toggled')
+    console.log("toggled");
     if (menuHidden) {
       setMenuHidden(false);
     } else {
       setMenuHidden(true);
     }
-  }
+  };
 
   return (
     <div className="Main">
       <ol className="menu">
-      <MenuItem hidden={false} label="Menu" onClick={toggleMenu}/>
-        {tabs.map(tab => (
+        <MenuItem hidden={false} label="Menu" onClick={toggleMenu} />
+        {tabs.map((tab) => (
           <MenuItem
             label={tab}
             key={tab}
             hidden={menuHidden}
             activeTab={activeTab === tab}
-            onClick={() => setActiveTab(tab)}
-          >
+            onClick={() => setActiveTab(tab)}>
             {tab}
-          </MenuItem>))}
+          </MenuItem>
+        ))}
       </ol>
       <div className="active-tab">
-        {children.map((child: any) => {
+        {children.map((child: JSX.Element) => {
           if (child.props.label !== activeTab) return undefined;
-          return <TabWrapper>{child}</TabWrapper>;
+          return <TabWrapper key={child.props.label}>{child}</TabWrapper>;
         })}
       </div>
     </div>
