678147afdc119ca027f64a4b9d87ea159722e27e
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-06-09 00:31:10 +0200

    chore: add mock deck for testing

---
+++ src/mocks/deck.ts
@@ -0,0 +1,280 @@
+import { ICard } from "../ts/types";
+
+export const mockDeck:ICard[] = [
+    {
+        "id": "green-stadium-shaded-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-squiggle-filled-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-stadium-shaded-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-stadium-blank-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-squiggle-filled-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-squiggle-shaded-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-stadium-blank-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-diamond-filled-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-stadium-filled-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-diamond-shaded-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-squiggle-blank-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-diamond-filled-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-diamond-shaded-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-squiggle-filled-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-diamond-filled-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-stadium-filled-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-diamond-shaded-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-squiggle-shaded-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-squiggle-filled-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-diamond-shaded-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-diamond-blank-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-stadium-blank-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-stadium-blank-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-diamond-blank-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-stadium-shaded-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-diamond-shaded-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-stadium-filled-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-diamond-shaded-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-diamond-filled-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-diamond-blank-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-diamond-filled-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-squiggle-shaded-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-squiggle-filled-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-stadium-shaded-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-stadium-filled-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-diamond-filled-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-squiggle-shaded-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-stadium-blank-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-squiggle-blank-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-diamond-blank-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-squiggle-shaded-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-diamond-filled-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-squiggle-filled-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-diamond-shaded-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-squiggle-blank-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-stadium-shaded-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-stadium-shaded-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-stadium-filled-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-diamond-blank-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-diamond-shaded-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-stadium-shaded-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-diamond-blank-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-squiggle-shaded-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-diamond-filled-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-stadium-filled-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-squiggle-blank-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-stadium-shaded-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-squiggle-shaded-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-squiggle-blank-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-squiggle-shaded-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-squiggle-filled-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-diamond-blank-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-stadium-shaded-3",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-squiggle-filled-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-squiggle-blank-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "violet-diamond-blank-2",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-stadium-blank-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "green-squiggle-blank-1",
+        "cardStatus": "Card-inactive"
+    },
+    {
+        "id": "red-stadium-filled-1",
+        "cardStatus": "Card-inactive"
+    }
+]
