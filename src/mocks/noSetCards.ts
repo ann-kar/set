cb3ea269f40b1454ab8c9d1770a084ef6702561e
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-06-09 03:57:54 +0200

    chore: add mock cards (no Set)

---
+++ src/mocks/noSetCards.ts
@@ -0,0 +1,14 @@
+export const noSetCards = [
+    "green-diamond-filled-1",
+    "green-diamond-filled-1",
+    "violet-diamond-shaded-1",
+    "violet-diamond-filled-1",
+    "red-squiggle-filled-0",
+    "red-squiggle-blank-2",
+    "red-squiggle-filled-2",
+    "violet-diamond-filled-2",
+    "red-squiggle-shaded-0",
+    "red-stadium-shaded-2",
+    "red-squiggle-shaded-0",
+    "red-stadium-shaded-0",
+  ];
